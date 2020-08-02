4b59a02209fecdfa3d28673c9fc4dcba92f61e4c
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-08-02 09:34:09 +0530

    backend fix

---
+++ utils.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from cv2 import cv2
+
+
+model=load_model("model/ferlatest.h5")
+
+def pipeline_model(path,filename):
+    '''img=cv2.imread(path)
+    predictions=model.predict(img)
+    testing_img=np.array(img)
+    testing_img=testing_img.reshape([48, 48])	
+    plt.gray()
+    plt.imshow(testing_img)
+    plt.show()
+    cv2.imwrite('static/predict/{}'.format(filename),img)'''
--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from flask import url_for,redirect
 import os
 from PIL import Image
 from keras.models import load_model
+from utils import pipeline_model
+
 
 
 
@@ -19,3 +21,8 @@ def emotion():
         filename=f.filename #name of file uploaded
         path=os.path.join(upload_folder,filename)
         f.save(path)
+        pipeline_model(path,filename)
+
+        return render_template("eotion.html",fileupload=True,img_name=filename)
+    return render_template("emotion.html",fileupload=False,img_name="random.png",w=300)
+
